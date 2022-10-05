6ee68b2b0cdc6a947ce52caec2e5897325c2d4e8
subramaniyant7 <tsubramaniyan2@gmail.com>
2022-10-05 22:20:14 +0530

    Images

--- resources/views/frontend/contact_us.blade.php
+++ resources/views/frontend/contact_us.blade.php
@@ -17,23 +17,21 @@
             <div class="container">
                 <h1>Contact Us</h1>
                 <div class="column-container column-count--2 align-center">
-
                     <div id="block-id-1643958633d4087687-0"
                         class="text-column align-center cc-fade-in-up cc-animate-init -in cc-animate-complete"
                         data-cc-animate="cc-fade-in-up" data-cc-animate-delay="0.15s" style="">
                         <div class="text-column__image text-column__image--shape-landscape">
                             <a
-                                href="https://goo.gl/maps/hMTHuBT28MEVFLAr9" aria-label="CHENNAI STORE">
+                                href="https://g.page/sai-ellora-nachiyarkoil?share" aria-label="Sailellora">
                                 <div class="rimage-outer-wrapper" style="max-width: 4608px">
                                     <div class="rimage-wrapper" style="padding-top:75.0%">
                                         <img class="rimage__image "
-                                            data-lazy-src="https://cdn.shopify.com/s/files/1/2270/8977/files/20220128_163158_{width}x.jpg?v=1643958797"
+                                            data-lazy-src="{{ URL::asset(FRONTEND.'/img/shop.jpeg') }}"
                                             loading="lazy" alt="" width="4608" height="3456"
-                                            src="https://cdn.shopify.com/s/files/1/2270/8977/files/20220128_163158_540x@2x.jpg?v=1643958797"
+                                            src="{{ URL::asset(FRONTEND.'/img/shop.jpeg') }}"
                                             data-lazy-loaded="true">
-
                                         <noscript>
-                                            <img src="https://cdn.shopify.com/s/files/1/2270/8977/files/20220128_163158_1024x1024.jpg?v=1643958797"
+                                            <img src="{{ URL::asset(FRONTEND.'/img/shop.jpeg') }}"
                                                 alt="" class="rimage__image ">
                                         </noscript>
                                     </div>
@@ -57,17 +55,18 @@
                     <div id="block-id-1643958633d4087687-1"
                         class="text-column align-center cc-fade-in-up cc-animate-init -in cc-animate-complete"
                         data-cc-animate="cc-fade-in-up" data-cc-animate-delay="0.3s" style="">
-                        <div class="text-column__image text-column__image--shape-landscape"><a href="/pages/contact-us"
+                        <div class="text-column__image text-column__image--shape-landscape">
+                            <a href="{{ url(FRONTENDURL.'contact_form') }}"
                                 aria-label="ONLINE CUSTOMER SUPPORT">
                                 <div class="rimage-outer-wrapper" style="max-width: 3960px">
                                     <div class="rimage-wrapper" style="padding-top:66.66666666666666%">
                                         <img class="rimage__image "
-                                            data-lazy-src="https://cdn.shopify.com/s/files/1/2270/8977/files/thumbs-up-on-yellow_{width}x.jpg?v=1643959654"
+                                            data-lazy-src="{{ URL::asset(FRONTEND.'/img/thumbs-up-on-yellow_540x@2x.webp')}}"
                                             loading="lazy" alt="" width="3960" height="2640"
-                                            src="https://cdn.shopify.com/s/files/1/2270/8977/files/thumbs-up-on-yellow_540x@2x.jpg?v=1643959654"
+                                            src="{{ URL::asset(FRONTEND.'/img/thumbs-up-on-yellow_540x@2x.webp')}}"
                                             data-lazy-loaded="true">
                                         <noscript>
-                                            <img src="https://cdn.shopify.com/s/files/1/2270/8977/files/thumbs-up-on-yellow_1024x1024.jpg?v=1643959654"
+                                            <img src="{{ URL::asset(FRONTEND.'/img/thumbs-up-on-yellow_540x@2x.webp')}}"
                                                 alt="" class="rimage__image ">
                                         </noscript>
                                     </div>
@@ -80,7 +79,7 @@
                         <div class="text-column__text rte align-center">
                             <p><strong>PHONE - +91 7200931501, +91 9443635502</strong></p>
                             <p>Operational Hours : 10 AM - 8 PM (Monday to Saturday)</p>
-                            <p>Email - support@wedtree.com</p>
+                            <p>Email - karunaveen86@gmail.com</p>
                         </div>
 
                         <div class="text-column__button-row align-center">
